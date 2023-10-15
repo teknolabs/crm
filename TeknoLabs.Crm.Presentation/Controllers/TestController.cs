b7bf37f4260df2ff7c515b801c770cc1af4b4451
Fatih ÇELEN <fatih.celen@mapeg.gov.tr>
2023-10-15 23:52:17 +0300

    Controller directory removed presentation layer

--- TeknoLabs.Crm.Presentation/Controllers/TestController.cs
+++ TeknoLabs.Crm.Presentation/Controllers/TestController.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
+using TeknoLabs.Crm.Presentation.Abstraction;
 
 namespace TeknoLabs.Crm.Presentation.Controllers
 {
-    internal class TestController
+    public sealed class TestController : ApiController
     {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("İşlem Başarılı");
+        }
     }
 }
--- TeknoLabs.Crm.WebApi/Program.cs
+++ TeknoLabs.Crm.WebApi/Program.cs
@@ -1,8 +1,10 @@
+using TeknoLabs.Crm.Presentation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddApplicationPart(typeof(AssemblyReferance).Assembly);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
