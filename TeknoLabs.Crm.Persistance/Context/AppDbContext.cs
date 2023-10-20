d95b348f5124440a1cfed78e9ec92eedf013de95
Fatih ÇELEN <fatihcelen@gmail.com>
2023-10-20 23:51:33 +0300

    appdbcontext add db settings

--- TeknoLabs.Crm.Persistance/Context/AppDbContext.cs
+++ TeknoLabs.Crm.Persistance/Context/AppDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using TeknoLabs.Crm.Domain.Abstract;
 using TeknoLabs.Crm.Domain.AppEntities;
 using TeknoLabs.Crm.Domain.AppEntities.Identity;
@@ -20,18 +21,29 @@ namespace TeknoLabs.Crm.Persistance.Context
             var entries = ChangeTracker.Entries<Entity>();
             foreach (var entry in entries)
             {
-                if (entry.State==EntityState.Added)
+                if (entry.State == EntityState.Added)
                 {
                     entry.Property(p => p.Id).CurrentValue = Guid.NewGuid().ToString();
                     entry.Property(p => p.CreatedDate).CurrentValue = DateTime.Now;
                 }
 
-                if (entry.State==EntityState.Modified)
+                if (entry.State == EntityState.Modified)
                 {
                     entry.Property(p => p.UpdatedDate).CurrentValue = DateTime.Now;
                 }
             }
             return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+        {
+            public AppDbContext CreateDbContext(string[] args)
+            {
+                var builder = new DbContextOptionsBuilder();
+                var connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TeknoLabsCrm;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+                builder.UseSqlServer(connectionString);
+                return new AppDbContext(builder.Options);
+            }
         }
     }
 }
---
+++ TeknoLabs.Crm.Persistance/Migrations/20231020204611_appdbcontextfactory.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeknoLabs.Crm.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class appdbcontextfactory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DatabaseName",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ServerName",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DatabaseName",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "ServerName",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Clients");
+        }
+    }
+}
