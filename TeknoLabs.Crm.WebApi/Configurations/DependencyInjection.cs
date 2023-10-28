cad8abbfc81cd23552a9837d9b04f510e6b0930c
Fatih ÇELEN <fatihcelen@gmail.com>
2023-10-28 23:14:37 +0300

    program cs cleıiıg

--- TeknoLabs.Crm.WebApi/Configurations/DependencyInjection.cs
+++ TeknoLabs.Crm.WebApi/Configurations/DependencyInjection.cs
@@ -1,5 +1,28 @@
-﻿namespace TeknoLabs.Crm.WebApi.Configurations;
+﻿using System.Reflection;
+
+namespace TeknoLabs.Crm.WebApi.Configurations;
 
 public static class DependencyInjection
 {
+    public static IServiceCollection InstallServices(this IServiceCollection services, IConfiguration configuration, params Assembly[] assemblies)
+    {
+        IEnumerable<IServiceInstaller> serviceInstallers = assemblies
+            .SelectMany(a => a.DefinedTypes)
+            .Where(IsAssignableToType<IServiceInstaller>)
+            .Select(Activator.CreateInstance)
+            .Cast<IServiceInstaller>();
+
+        foreach (var serviceInstaller in serviceInstallers)
+        {
+            serviceInstaller.Install(services, configuration);
+        }
+
+        return services;
+
+        static bool IsAssignableToType<T>(TypeInfo typeInfo) =>
+        typeInfo.IsAssignableFrom(typeof(T)) &&
+        !typeInfo.IsInterface &&
+        !typeInfo.IsAbstract;
+
+    }
 }
--- TeknoLabs.Crm.WebApi/Program.cs
+++ TeknoLabs.Crm.WebApi/Program.cs
@@ -1,9 +1,8 @@
+using TeknoLabs.Crm.WebApi.Configurations;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
-
-
-
+builder.Services.InstallServices(builder.Configuration, typeof(IServiceInstaller).Assembly);
 
 var app = builder.Build();
 
